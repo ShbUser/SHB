94d5cfbe9c42033028d3ab9b33c2f7606db7f03d
ShbUser <90904467+ShbUser@users.noreply.github.com>
2022-08-14 11:45:48 +0530

    add&view

--- ECommerce_SHB/helpers/product-helpers.js
+++ ECommerce_SHB/helpers/product-helpers.js
@@ -1,23 +1,46 @@
 
 const { ObjectId } = require('mongodb')
-var db=require('../config/Connection')
-var collection=require('../config/collections')
+var db = require('../config/Connection')
+var collection = require('../config/collections')
 const collections = require('../config/collections')
 const { promise } = require('bcrypt/promises')
 const { resolve, reject } = require('promise')
 const { response } = require('express')
 const res = require('express/lib/response')
 const { errorFunc } = require('express-fileupload/lib/utilities')
-let objectID=require('mongodb').ObjectId
+let objectID = require('mongodb').ObjectId
 
 
 
-module.exports={
-    addProduct:(Product,callback)=>{
+module.exports = {
+    addProduct: (Product, callback) => {
 
-        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(Product).then((data)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(Product).then((data) => {
             callback(data.insertedId)
 
         })
-    }
+    },
+
+    addCategory: (categry) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.CATEGORY_COLLECTION).insertOne(categry).then((data) => {
+                console.log(categry)
+                resolve(data)
+            })
+        })
+    },
+    getCategory: () => {
+        return new Promise(async(resolve, reject) => {
+            let categories=await db.get().collection(collection.CATEGORY_COLLECTION).find().toArray()
+            resolve(categories  )
+        })            
+    },
+
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+           let product= await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+           
+           resolve(product)
+        })
+    },
 }
--- ECommerce_SHB/routes/admin.js
+++ ECommerce_SHB/routes/admin.js
@@ -1,17 +1,44 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 let productHelper = require('../helpers/product-helpers')
 
 
 /* GET home page. */
-
 router.get('/', (req, res) => {
-    res.render('admin/admin_home',{admin:true})
-})
-router.get('/add_products', (req, res) => {
-    res.render('admin/add_products')
+    // productHelper.getCategory().then((response)=>{
+    //     console.log(response);
+    // res.render('admin/add_products', { admin: true,response })
+    //res.render('admin/add_categories', { admin: true })
+    productHelper.getAllProducts().then((products) => {
+        console.log(products);
+        res.render('admin/view_products', { admin: true, products });
+    })
 })
 
+router.get('/get_products', (req, res) => {
+    productHelper.getCategory().then((response) => {
+        let cat = response.name
+        res.render('admin/add_products', { admin: true, cat })
+    })
+})
+
+router.get('/get_categories', (req, res) => {
+
+    res.render('admin/add_categories', { admin: true })
+})
+
+router.get('/view_products', (req, res) => {
+
+    res.render('admin/view_products', { admin: true })
+})
+
+router.get('/view_products', (req, res) => {
+    productHelper.getAllProducts().then((products) => {
+        console.log(products);
+        res.render('admin/view-products', { admin: true, products });
+    })
+})
 
 router.post('/add-product', (req, res) => {
     productHelper.addProduct(req.body, (id) => {
@@ -22,9 +49,19 @@ router.post('/add-product', (req, res) => {
                 res.redirect('/admin')
             else console.log(err)
         })
-
-
     })
 })
+
+
+router.post('/add-categories', (req, res) => {
+    console.log(req.body);
+    productHelper.addCategory(req.body).then((response) => {
+        res.redirect('/admin')
+    })
+
+})
+
+
+
 
 module.exports = router;
