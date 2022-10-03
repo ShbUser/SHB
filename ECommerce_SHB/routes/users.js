97bd3e8bd914963836cb70967151771130b79c50
ShbUser <90904467+ShbUser@users.noreply.github.com>
2022-10-03 10:30:47 +0530

    invoiceamt

--- ECommerce_SHB/routes/users.js
+++ ECommerce_SHB/routes/users.js
@@ -377,7 +377,8 @@ router.get('/invoice/:id', verifyLogin, async (req, res, next) => {
         let disc = coupon.coupendiscount
         let totalAmount
         if (disc != undefined) {
-          totalAmount = parseInt(order.deliveryDetails.totalAmount) - parseInt(disc)
+          totalAmount= parseInt(order.deliveryDetails.totalAmount) + parseInt(disc)
+          totalAmount = parseInt(totalAmount) - parseInt(disc)
         } else {
           disc = "0.00"
           totalAmount = order.deliveryDetails.totalAmount
